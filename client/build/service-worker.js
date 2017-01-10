aa4dbd4880a486a89bab357189a6dc3f45486aed
Jacob Mortelliti <jacob.harrowmortelliti@maine.eu>
2017-01-09 22:04:40 -0500

    updated serviceworker to have the correct path

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -3,7 +3,7 @@ self.addEventListener('push', (event) => {
 
   const title  = 'Donald Trump tweeted:' 
   const body = content.body
-  const icon = iconPath
+  const icon = '/DJT_Headshot_V2_normal.jpg'
 
   event.waitUntil(
     self.registration.showNotification(title, { body, icon })
